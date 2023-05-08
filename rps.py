d0f409710ce80147329c3e73c27d4cebfc59ea1e
ossims <113637544+ossims@users.noreply.github.com>
2023-05-08 13:48:04 -0400

    Add files via upload

---
+++ rps.py
@@ -0,0 +1,38 @@
+import random
+
+def get_choices():
+  player_choice = input("Enter a choice (rock, paper, scissors: ")
+  options = ["rock", "paper", "scissors"]
+  computer_choice = random.choice(options)
+  choices = {"player": player_choice, "computer": computer_choice}
+  return choices
+
+def check_win(player, computer):
+  print(f"You chose {player} and the computer chose {computer}.")
+  
+  if player == computer:
+    return "Tie" 
+
+  
+  elif player == "rock":
+    if computer == "scissors":
+      return "You win"
+    else:
+      return "Computer wins"
+
+  elif player == "scissors":
+    if computer == "paper":
+      return "You win"
+    else:
+      return "Computer wins"
+
+  elif player == "paper":
+    if computer == "rock":
+      return "You win"
+    else:
+      return "Computer wins"
+
+choices = get_choices()
+result = check_win (choices["player"], choices ["computer"])
+print(result)
+
